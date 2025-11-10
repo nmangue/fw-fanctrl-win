7a701fa892c88cc6cd939f51187c52c48a98defa
Nicolas MANGUÉ <nicolas.mangue@gmail.com>
2025-11-10 16:23:44 +0100

    Working prototype

--- src/FrameworkFanControl/Worker.cs
+++ src/FrameworkFanControl/Worker.cs
@@ -1,16 +1,43 @@
+using FrameworkFanControl.Infrastructure;
+
 namespace FrameworkFanControl;
 
 public class Worker(ILogger<Worker> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        using IStateProvider _stateProvider = new LhmStateProvider();
+        IFanControlProfile _fanProfile = new LinearFanControlCurve(new Dictionary<float, Percentage>
         {
-            if (logger.IsEnabled(LogLevel.Information))
+            { 50f, 00 },
+            { 60f, 50 },
+            { 80f, 80 }
+        });
+        using IFanController _fanController = new CrosEcFanController();
+
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
             {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                if (logger.IsEnabled(LogLevel.Information))
+                {
+                    logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
+
+                var state = _stateProvider.ReadState();
+                logger.LogInformation("CPU Max at {temp}°C", state.CoreMaxTemp);
+                var fanSpeed = _fanProfile.Get(state);
+                logger.LogInformation("Setting speed to {speed}", fanSpeed);
+                _fanController.SetFanDuty(fanSpeed);
+
+                await Task.Delay(5000, stoppingToken);
             }
-            await Task.Delay(1000, stoppingToken);
         }
+        finally
+        {
+            logger.LogInformation("Restoring automatic fan control");
+            _fanController.ActivateAutoFanContrl();
+        }
+
     }
 }
